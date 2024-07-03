3e321da0a897aff5f36064d37edb928b0f42574b
skumar <skumar@klick.com>
2024-07-03 15:08:15 -0400

    Recaptcha

--- blocks/recaptcha/recaptcha.js
+++ blocks/recaptcha/recaptcha.js
@@ -1,10 +1,7 @@
-import { getConfigValue } from './configs.js';
+import { getConfigValue } from '../../scripts/configs.js';
 
 export default function decorate(block) {
-  const envid= getConfigValue('env');
-  const sitekey= getConfigValue('site_key');
-  const sitesecret= getConfigValue('site_secret');
-  console.log("---ReCaptcha----> {} | {} | {} ",envid,sitekey,sitesecret);
+  getConfigs();
 
 }
 
@@ -14,5 +11,5 @@ async function getConfigs() {
   const envid=await getConfigValue('env');
   const sitekey=await getConfigValue('site_key');
   const sitesecret=await getConfigValue('site_secret');
-  console.log("-------> {} | {} | {} ",envid,sitekey,sitesecret);
+  console.log("---ReCaptcha----> {} | {} | {} ",envid,sitekey,sitesecret);
 }
