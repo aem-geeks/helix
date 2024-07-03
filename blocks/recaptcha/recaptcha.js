4cb69761b21472d23a06b8f278f3b79ae3011f7d
skumar <skumar@klick.com>
2024-07-03 15:34:26 -0400

    Recaptcha

--- blocks/recaptcha/recaptcha.js
+++ blocks/recaptcha/recaptcha.js
@@ -2,20 +2,21 @@ import { getConfigValue } from '../../scripts/configs.js';
 
 export default async function decorate(block) {
   getgScript();
-  getConfigs()
+  const site_key=getConfigs('site_key')
   const captchadiv = document.createElement('div');
   const indiv=document.createElement('div')
   indiv.setAttribute('class','g-recaptcha');
-  indiv.setAttribute('data-sitekey','6Lc2fLEbAAAAADmlICdDP4r8obxMsMmOd1j6teuT');
+  indiv.setAttribute('data-sitekey',site_key);
   block.appendChild(indiv);
 }
 
 
-async function getConfigs() {
+async function getConfigs(key) {
   const envid=await getConfigValue('env');
-  const sitekey=await getConfigValue('site_key');
+  const sitekey=await getConfigValue(key);
   const sitesecret=await getConfigValue('site_secret');
   console.log("---ReCaptcha----> {} | {} | {} ",envid,sitekey,sitesecret);
+  return sitekey;
 }
 
 async function getgScript(){
